757c86edea3c527aa36ca29079a8d381d279a353
hyperflow <hyperflow@kakao.com>
2020-12-17 02:48:57 +0900

    feat(FrontPage): Implement frontpage

---
+++ src/page/FrontPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import EthChart from '../component/EthChart';
+import QuickContainer from '../component/QuickContainer';
+import Ralcuranbo from '../component/Ralcuranbo';
+
+const FrontPage = () => {
+  return (
+    <>
+      <QuickContainer />
+      <br />
+      <Ralcuranbo />
+      <br />
+      <EthChart />
+    </>
+  );
+};
+
+export default FrontPage;
